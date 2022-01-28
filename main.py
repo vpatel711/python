b21f982db4456ac3a4d1a2482bfa320bb62d848d
vpatel711 <98567280+vpatel711@users.noreply.github.com>
2022-01-27 20:04:11 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+print("this is python tutorial")
+print(3.14)
+print(True)
+print("34")
+print(2+3j)
+# print(False)
+print("hello world")
+print("i am trying to buy home")
+print('''i am in
+    the process of 
+    buying a home''')
+print( ''' i am in 
+the process of 
+buying a home, 
+i want to learn python 
+and it is 
+not that easy''')
+
+
+
