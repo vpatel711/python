b559d2604dd9e7c94b0a110794c39c34c114afde
vpatel711 <98567280+vpatel711@users.noreply.github.com>
2022-01-27 20:01:55 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+ breakpoint in the code line below to debug your script.
+    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+# coding=utf-8
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+num1 = int(input("Enter the first number"))
+num2 = int(input("Enter the second number"))
+op = input("Enter the operator")
+if (op=="+")
+    print(num1+num2)
+elif (op=="-")
+    print(num1-num2)
+elif (op=="*")
+    print(num1*num2)
+elif (op=="/")
+    print(num1/num2)
+else:
+    print("invalid operator")
+
